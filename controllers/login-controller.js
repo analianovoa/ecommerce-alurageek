a951a683776f75373d547a687bd808f091ee2eb4
Analia Novoa <110686057+analianovoa@users.noreply.github.com>
2025-03-05 10:38:50 -0300

    Update login-controller.js

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -6,7 +6,7 @@ form.addEventListener("submit" , (evento) => {
     const paswword = document.querySelector("[data-password]").value;
 
     if (email.length > 6 && paswword.length > 6 ) {
-        window.location.href = "http://../productos.html" ;
+        window.location.href = "productos.html" ;
 
     }else {
         alert ("Por favor ingrese mas de 6 caracteres en email y contraseña");
